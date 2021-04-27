1af1aa99919a42ed6ea977f6c5e0e51a592e31e5
Brito <>
2021-04-27 12:07:45 +0000

    Exemplo de gerador com multiplas opções

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-#!/usr/bin/python3
+from random import randint, random
+from time import time
 
-dic = {'Name': 'Turtle', 'Age': 7}
 
-print ("Nome : {: >14}".format(dic.get('Name', "Never")))
-print ("Idade : {: >8}".format(dic.get('Age')))
-print ("Escolaridade : {}".format(dic.get('Education', "Never")))
+def gen_random(op):
+    try:
+        op = int(op)
+        yield {
+            1: time(),
+            2: random(),
+            3: randint(1, 10),
+        }.get(op)
+    except:
+        raise
+
+
+if __name__ == '__main__':
+    a = gen_random(1)
+    b = gen_random('2')
+    c = gen_random('3')
+
+    print(next(a))
+    print(next(b))
+    print(next(c))
+    for i in range(10):
+        print(next(a))
